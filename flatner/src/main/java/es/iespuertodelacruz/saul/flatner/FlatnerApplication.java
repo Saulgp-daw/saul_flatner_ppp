404733c090865ad2406e7ff43052637769ea99c3
Saulgp-daw <saulgp4a@gmail.com>
2024-02-07 19:15:38 +0000

    Primer commit, creación del proyecto springboot

---
+++ flatner/src/main/java/es/iespuertodelacruz/saul/flatner/FlatnerApplication.java
@@ -0,0 +1,13 @@
+package es.iespuertodelacruz.saul.flatner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlatnerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlatnerApplication.class, args);
+	}
+
+}
---
+++ flatner/src/test/java/es/iespuertodelacruz/saul/flatner/FlatnerApplicationTests.java
@@ -0,0 +1,13 @@
+package es.iespuertodelacruz.saul.flatner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlatnerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
