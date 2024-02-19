14bb7d1448d6233f17cbb642330761f430761632
Saulgp-daw <saulgp4a@gmail.com>
2024-02-19 18:27:07 +0000

    feat(PROYECTO): Los usuarios puedes subir imágenes para su perfil utilizando base64 y permitiendo tener cada usuario su propia carpeta que resultará útil más adelante si quieren subir imágenes de pisos

---
+++ flatner_app/App.tsx
@@ -0,0 +1,41 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+import 'react-native-gesture-handler';
+import React, { useEffect } from 'react';
+import type {PropsWithChildren} from 'react';
+
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+//import { dataSource } from './src/data/Database';
+
+
+function App(): JSX.Element {
+
+  // useEffect(() => {
+  //   async function iniciarDDBB(){
+  //     await dataSource.initialize();
+  //   }
+  //   iniciarDDBB();
+  // }, []);
+
+
+  return (
+    <View>
+    <Text>Hola :)</Text>
+    </View> 
+  );
+}
+
+export default App;
