c927aa89f284939dab91389a84d66cd246125a16
Saulgp-daw <saulgp4a@gmail.com>
2024-03-03 19:41:49 +0000

    feat(PRYECTO): Modificada la bbdd para adaptarla mejor a las necesidades de los usuarios

--- flatner/src/main/resources/flatner.sql
+++ flatner/src/main/resources/flatner.sql
@@ -1,6 +1,8 @@
-DROP TABLE IF EXISTS usuarios;
-DROP TABLE IF EXISTS pisos;
 DROP TABLE IF EXISTS watchlists;
+DROP TABLE IF EXISTS pisos;
+DROP TABLE IF EXISTS usuarios;
+
+
 
 CREATE TABLE pisos (
     id_piso int AUTO_INCREMENT,
@@ -9,7 +11,8 @@ CREATE TABLE pisos (
     titulo varchar(255) NOT NULL,
     descripcion varchar(255) DEFAULT NULL,
     num_habitaciones int NOT NULL,
-    valoracion decimal(2,1) DEFAULT NULL,
+    valoracion decimal(2,1) DEFAULT 2.5,
+    num_votos int default 1,
     m_cuadrados int NOT NULL,
     fotos varchar(255) DEFAULT NULL,
     ubicacion varchar(550) NOT NULL,
@@ -26,6 +29,7 @@ CREATE TABLE pisos (
     gas_incluido tinyint(1) DEFAULT 0,
     propietario_reside tinyint(1) DEFAULT 0,
     wifi tinyint(1) DEFAULT 0,
+    verified tinyint(1) DEFAULT 0,
     CONSTRAINT pk_pisos PRIMARY KEY(id_piso)
 );
 
@@ -38,8 +42,10 @@ CREATE TABLE usuarios (
     sexo varchar(10) DEFAULT NULL,
     anho_nacimiento int DEFAULT NULL,
     foto_perfil varchar(255) DEFAULT NULL,
-    valoracion decimal(2,1) DEFAULT NULL,
-    active tinyint(1) DEFAULT NULL,
+    valoracion decimal(2,1) DEFAULT 2.5,
+    num_votos int default 1,
+    active tinyint(1) DEFAULT 0,
+    verified tinyint(1) DEFAULT 0,
     hash varchar(255) DEFAULT NULL,
     id_piso_actual int DEFAULT NULL,
     fecha_ultima_estancia bigint DEFAULT NULL,
@@ -51,6 +57,7 @@ CREATE TABLE watchlists (
     id_watchlist int AUTO_INCREMENT,
     email_usuario varchar(255) NOT NULL,
     id_piso int NOT NULL,
+    anotaciones varchar(500) NOT NULL,
     CONSTRAINT pk_id_watchlist PRIMARY KEY (id_watchlist),
     CONSTRAINT fk_usuarios FOREIGN KEY(email_usuario) REFERENCES usuarios(email),
     CONSTRAINT fk_pisos FOREIGN KEY(id_piso) REFERENCES pisos(id_piso)
